b8d4457a32cafd0ebb07b172241faf6f761102c8
Vitor Alves Rocha <vitoralvesrocha16@gmail.com>
2017-03-12 23:53:42 -0300

    Primeira versão do Blog

---
+++ frontend/resources/js/app.js
@@ -0,0 +1,53 @@
+// Cria o modulo e seu nome: leadBlogApp
+var leadBlogApp = angular.module('leadBlogApp', ['ngRoute']);
+
+// Configura rotas
+leadBlogApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	$locationProvider.hashPrefix('');
+	$routeProvider
+
+		// rota da pagina inicial
+		.when('/', {
+			templateUrl : 'pages/home.html',
+			controller  : 'mainController'
+		})
+
+		// rota da pagina sobre
+		.when('/about', {
+			templateUrl : 'pages/about.html',
+			controller  : 'aboutController'
+		})
+
+		// rota da pagina contato
+		.when('/contact', {
+			templateUrl : 'pages/contact.html',
+			controller  : 'contactController'
+		})
+
+		.otherwise({
+			redirectTo:'/'
+		});
+}]);
+
+leadBlogApp.controller('mainController', function($scope) {
+	$scope.message = 'Everyone come and see how good I look!';
+
+	$('.carousel.carousel-slider').carousel({
+		fullWidth: true,
+		duration: 300
+	});
+
+	setInterval(function(){
+		$('.carousel').carousel('next');
+	}, 5000);
+
+	$(".button-collapse").sideNav();
+});
+
+leadBlogApp.controller('aboutController', function($scope) {
+	$scope.message = 'Look! I am an about page.';
+});
+
+leadBlogApp.controller('contactController', function($scope) {
+	$scope.message = 'Contact us! JK. This is just a demo.';
+});

74fbef35a872ad4d00e124ab04e16ca49895cdad
Vinicius Dias Villar <diasvillar.vinicius@gmail.com>
2017-03-14 03:22:38 -0300

    validaç~ao dos dados do cadastro

--- static/js/app.js
+++ static/js/app.js
@@ -92,7 +92,7 @@ leadBlogApp.controller('registerController', function($scope, $http, $location) 
 
         //enviando as variáveis do cliente pro servidor
         $http({
-            url: "https://seorganizareventos.herokuapp.com/api", //http://127.0.0.1:5000/api
+            url: "http://127.0.0.1:5000/api", //https://seorganizareventos.herokuapp.com/api
             method: "POST",
             headers: { 'Content-Type': 'application/json' },
             data: jsonString
